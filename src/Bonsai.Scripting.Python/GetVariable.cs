71de8804ae24b58d0a566702764c3a5114956253
glopesdev <glopesdev@users.noreply.github.com>
2023-01-26 11:11:14 +0000

    Add support to get and set scope variables

---
+++ src/Bonsai.Scripting.Python/GetVariable.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.ComponentModel;
+using System.Linq;
+using System.Reactive.Linq;
+using Python.Runtime;
+
+namespace Bonsai.Scripting.Python
+{
+    public class GetVariable : Source<PyObject>
+    {
+        [TypeConverter(typeof(ScopeNameConverter))]
+        public string ScopeName { get; set; }
+
+        public string VariableName { get; set; }
+
+        public override IObservable<PyObject> Generate()
+        {
+            return RuntimeManager.RuntimeSource.SelectMany(runtime =>
+            {
+                var scope = runtime.Resources.Load<PyModule>(ScopeName);
+                return Observable.Return(scope.Get(VariableName));
+            });
+        }
+
+        public IObservable<PyObject> Generate<TSource>(IObservable<TSource> source)
+        {
+            return RuntimeManager.RuntimeSource.SelectMany(runtime =>
+            {
+                var scope = runtime.Resources.Load<PyModule>(ScopeName);
+                return source.Select(_ =>
+                {
+                    using (Py.GIL())
+                    {
+                        return scope.Get(VariableName);
+                    }
+                });
+            });
+        }
+    }
+}
---
+++ src/Bonsai.Scripting.Python/SetVariable.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.ComponentModel;
+using System.Linq;
+using System.Reactive.Linq;
+using Python.Runtime;
+
+namespace Bonsai.Scripting.Python
+{
+    public class SetVariable : Sink
+    {
+        [TypeConverter(typeof(ScopeNameConverter))]
+        public string ScopeName { get; set; }
+
+        public string VariableName { get; set; }
+
+        public override IObservable<TSource> Process<TSource>(IObservable<TSource> source)
+        {
+            return RuntimeManager.RuntimeSource.SelectMany(runtime =>
+            {
+                var scope = runtime.Resources.Load<PyModule>(ScopeName);
+                return source.Do(value =>
+                {
+                    using (Py.GIL())
+                    {
+                        scope.Set(VariableName, value);
+                    }
+                });
+            });
+        }
+    }
+}
