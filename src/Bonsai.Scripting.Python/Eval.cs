70589a8c06f53b1661c7c57627227e27fa7c64c2
glopesdev <glopesdev@users.noreply.github.com>
2023-01-26 11:11:49 +0000

    Add support for evaluating python expressions

---
+++ src/Bonsai.Scripting.Python/Eval.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.ComponentModel;
+using System.Linq;
+using System.Reactive.Linq;
+using Python.Runtime;
+
+namespace Bonsai.Scripting.Python
+{
+    public class Eval : Combinator<PyObject>
+    {
+        [TypeConverter(typeof(ScopeNameConverter))]
+        public string ScopeName { get; set; }
+
+        public string Expression { get; set; }
+
+        public override IObservable<PyObject> Process<TSource>(IObservable<TSource> source)
+        {
+            return RuntimeManager.RuntimeSource.SelectMany(runtime =>
+            {
+                var scope = runtime.Resources.Load<PyModule>(ScopeName);
+                return source.Select(_ =>
+                {
+                    using (Py.GIL())
+                    {
+                        return scope.Eval(Expression);
+                    }
+                });
+            });
+        }
+    }
+}
