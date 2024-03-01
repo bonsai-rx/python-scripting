d8b48c494e3654e41d2c7ced22ba70d8450d6b69
glopesdev <glopesdev@users.noreply.github.com>
2024-03-01 22:23:21 +0000

    Add operator to force notifications inside the GIL

---
+++ src/Bonsai.Scripting.Python/ObservableExtensions.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Reactive;
+using System.Reactive.Linq;
+using Python.Runtime;
+
+namespace Bonsai.Scripting.Python
+{
+    static class ObservableExtensions
+    {
+        public static IObservable<TSource> ObserveOnGIL<TSource>(this IObservable<TSource> source)
+        {
+            return Observable.Create<TSource>(observer =>
+            {
+                var sourceObserver = Observer.Create<TSource>(
+                    value =>
+                    {
+                        using (Py.GIL())
+                        {
+                            observer.OnNext(value);
+                        }
+                    },
+                    observer.OnError,
+                    observer.OnCompleted);
+                return source.SubscribeSafe(sourceObserver);
+            });
+        }
+    }
+}
---
+++ src/Bonsai.Scripting.Python/ObserveOnGIL.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.ComponentModel;
+using System.Linq;
+using System.Reactive.Linq;
+
+namespace Bonsai.Scripting.Python
+{
+    /// <summary>
+    /// Represents an operator that wraps the source sequence to ensure all notifications
+    /// are emitted while holding the Python global interpreter lock.
+    /// </summary>
+    [Description("Wraps the source sequence to ensure all notifications are emitted while holding the Python global interpreter lock.")]
+    public class ObserveOnGIL : Combinator
+    {
+        /// <summary>
+        /// Wraps an observable sequence to ensure all notifications are emitted
+        /// while holding the Python global interpreter lock.
+        /// </summary>
+        /// <typeparam name="TSource">
+        /// The type of the elements in the <paramref name="source"/> sequence.
+        /// </typeparam>
+        /// <param name="source">The source sequence to wrap.</param>
+        /// <returns>
+        /// An observable sequence that is identical to the <paramref name="source"/>
+        /// sequence but where there is an additional side effect of ensuring that
+        /// all notifications are emitted inside the Python global interpreter lock.
+        /// </returns>
+        public override IObservable<TSource> Process<TSource>(IObservable<TSource> source)
+        {
+            return RuntimeManager.RuntimeSource.SelectMany(_ => source.ObserveOnGIL());
+        }
+    }
+}
